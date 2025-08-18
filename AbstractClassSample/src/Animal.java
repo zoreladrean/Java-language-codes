f32b9c768be3c5c7e1fb3a0effcfac195b07902d
Zorel Adrean R. Java <petrovamario@gmail.com>
2025-08-18 20:24:38 +0800

    add superclass Animal

---
+++ AbstractClassSample/src/Animal.java
@@ -0,0 +1,14 @@
+public abstract class Animal {
+
+  private String name;
+  
+  public abstract void speak ( );
+
+  public String getName ( ) {
+    return name;
+  }
+
+  public void setName ( String name ) {
+    this.name = name;
+  }
+}
