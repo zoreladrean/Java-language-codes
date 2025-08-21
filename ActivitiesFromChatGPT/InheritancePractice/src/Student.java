39358aac2a7789776f8fd00897f06faeccd08c67
Zorel Adrean R. Java <petrovamario@gmail.com>
2025-08-21 21:56:47 +0800

    edit student subclass

--- ActivitiesFromChatGPT/InheritancePractice/src/Student.java
+++ ActivitiesFromChatGPT/InheritancePractice/src/Student.java
@@ -2,11 +2,11 @@ public class Student extends Person {
   int grade;
 
   public Student ( String name, int age, int grade ) {
-    this.name = name;
-    this.age = age;
+    super ( name, age );
     this.grade = grade;
   }
 
+  @Override
   public void introduce ( ) {
     System.out.println( "Hi, my name is " + name + " and I am " + age + " years old, and I am in grade " + grade + "." );
   }
