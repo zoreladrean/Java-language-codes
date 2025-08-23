38c83a8969d205418fb6a18b21e2e1de5739c372
Zorel Adrean R. Java <petrovamario@gmail.com>
2025-08-23 20:15:25 +0800

    add Circle subclass

---
+++ ActivitiesFromSchool/BasicShapeInheritance/src/Circle.java
@@ -0,0 +1,15 @@
+public class Circle extends Shape {
+  double radius;
+  
+  public Circle (double radius) {
+    this.radius = radius;
+  }
+
+  public double area(){
+    return(Math.PI  * (radius * radius));
+  }
+
+  public double perimeter(){
+    return(2*Math.PI*radius);
+  }
+}
