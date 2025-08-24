caff69a2986166f1ff2f593f101cc98f5fae7051
Zorel Adrean R. Java <petrovamario@gmail.com>
2025-08-24 22:02:13 +0800

    modified main method

--- ActivitiesFromSchool/GenericProductStorage/src/Main.java
+++ ActivitiesFromSchool/GenericProductStorage/src/Main.java
@@ -1,3 +1,48 @@
+import java.util.Scanner;
+
 public class Main {
-  
+
+  public static void main(String[] args) {
+    
+    Scanner s = new Scanner ( System.in );
+
+    System.out.print ( "Test case: " );
+    int c = s.nextInt();
+
+    if ( c == 1 )
+      test1 ( );
+    else
+      if ( c == 2 )
+        test2 ( );
+  }
+
+  public static void test1 ( ) {
+
+    Pen p = new Pen();
+    Apple a = new Apple();
+    Pineapple pa = new Pineapple();
+
+    System.out.println(p);
+    System.out.println(p);
+    System.out.println(pa);
+  }
+
+  public static void test2 ( ) {
+
+    Pen p = new Pen();
+    Apple a = new Apple();
+    Pineapple pa = new Pineapple();
+
+    Product <Pen> prP = new Product<>();
+    prP.setP(p);
+    System.out.println(prP);
+
+    Product <Apple> prA = new Product<>();
+    prA.setP(a);
+    System.out.println(prA);
+
+    Product <Pineapple> prPa = new Product<>();
+    prPa.setP(pa);
+    System.out.println(prPa);
+  }
 }
