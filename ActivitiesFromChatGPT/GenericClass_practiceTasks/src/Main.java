5c139e20905350808a136536c0eae8f8a98c3279
Zorel Adrean R. Java <petrovamario@gmail.com>
2025-08-29 22:38:37 +0800

    edit main method

--- ActivitiesFromChatGPT/GenericClass_practiceTasks/src/Main.java
+++ ActivitiesFromChatGPT/GenericClass_practiceTasks/src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Box <Boolean> box1 = new Box<>();
+        box1.setValue(true);
+        System.out.println(box1.getValue());
+
+        System.out.println();
+
+        Box <Double> box2 = new Box<>();
+        box2.setValue(3.14);
+        System.out.println(box2.getValue());
+
+        System.out.println();
+
+        Box <String> box3 = new Box<>();
+        box3.setValue("Hello World!");
+        System.out.println("The box contains: " +box3.getValue());
     }
 }
