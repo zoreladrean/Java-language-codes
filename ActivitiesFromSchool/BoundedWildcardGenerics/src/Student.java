66ae9185cd577a4272303eddd8107beb982a1525
Zorel Adrean R. Java <petrovamario@gmail.com>
2025-09-09 19:52:25 +0800

    add Student class

---
+++ ActivitiesFromSchool/BoundedWildcardGenerics/src/Student.java
@@ -0,0 +1,10 @@
+public class Student extends Person {
+  
+  public Student ( String name ) {
+    super ( name );
+  }
+
+  public void study ( ) {
+    System.out.println ( name + " is studying..." );
+  }
+}
