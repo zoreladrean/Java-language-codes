6f9330c0b2acdc2be94dcc7127a3fbc9fd85f4cd
Zorel Adrean R. Java <petrovamario@gmail.com>
2025-09-09 19:53:12 +0800

    add CCS class

---
+++ ActivitiesFromSchool/BoundedWildcardGenerics/src/CCS.java
@@ -0,0 +1,10 @@
+public class CCS extends Student {
+
+  public CCS ( String name ) {
+    super ( name );
+  }
+
+  public void code ( ) {
+    System.out.println ( name + " is coding..." );
+  }
+}
