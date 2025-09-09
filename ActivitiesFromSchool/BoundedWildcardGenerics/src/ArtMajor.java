0cf7da238358fb59c8c667e690190babd762344b
Zorel Adrean R. Java <petrovamario@gmail.com>
2025-09-09 19:52:49 +0800

    add ArtMajor class

---
+++ ActivitiesFromSchool/BoundedWildcardGenerics/src/ArtMajor.java
@@ -0,0 +1,11 @@
+public class ArtMajor extends Student {
+
+  public ArtMajor ( String name ) {
+    super ( name );
+  }
+
+  public void doArt ( ) {
+    System.out.println ( name + " is doing Art..." );
+  }
+
+}
