3e16496d0a1f7d47ee44495bc61767568de1b583
Zorel Adrean R. Java <petrovamario@gmail.com>
2025-09-09 19:52:00 +0800

    add Army class

---
+++ ActivitiesFromSchool/BoundedWildcardGenerics/src/Army.java
@@ -0,0 +1,10 @@
+public class Army extends Person {
+
+  public Army ( String name ) {
+    super ( name );
+  }
+
+  public void shoot ( ) {
+    System.out.println ( name + " is shooting..."  );
+  }
+}
