ca503c7531b01b5cc97418c585a9a9f7687cf4d3
Zorel Adrean R. Java <petrovamario@gmail.com>
2025-09-11 21:14:40 +0800

    add main method of abstract class with interface program sample

---
+++ AbstractClassWithInterfaceSample/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main ( String [ ] args ) {
+
+        WorkingDog aSheepHerder = new WorkingDog ( );
+        WorkingDog aSeeingEyeDog = new WorkingDog ( );
+
+        aSheepHerder.setName ( "Simon, the Border Collie" );
+        aSeeingEyeDog.setName ( "Sophie, the German Shepherd" );
+
+        aSheepHerder.setHoursOfTraining ( 40 );
+        aSeeingEyeDog.setHoursOfTraining ( 300 );
+
+        System.out.println ( aSheepHerder.getName ( ) + " says " );
+        aSheepHerder.speak ( );
+        aSheepHerder.work ( );
+
+        System.out.println ( );
+
+        System.out.println ( aSeeingEyeDog.getName ( ) + " says " );
+        aSeeingEyeDog.speak ( );
+        aSeeingEyeDog.work ( );
+    }
+}
