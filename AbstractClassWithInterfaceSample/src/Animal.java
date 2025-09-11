8b2b971f344961c9330917f44367a0efad517dbd
Zorel Adrean R. Java <petrovamario@gmail.com>
2025-09-11 21:15:06 +0800

    add Animal abstract class

---
+++ AbstractClassWithInterfaceSample/src/Animal.java
@@ -0,0 +1,16 @@
+public abstract class Animal {
+
+  private String name;
+
+  public abstract void speak ( );
+
+  public String getName ( ) {
+
+    return name;
+  }
+
+  public void setName ( String name ) {
+
+    this.name = name;
+  }
+}
